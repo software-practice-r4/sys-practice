c100123036ad791a613adeb9c5f581d3a915ba3f
kouta <kouta@LAPTOP-NTLO6BU2>
2022-07-04 21:26:16 +0900

    DBへの接続を受け取った形式に変更
    getRemoteConnectionが何度も呼び出されていたため1回のみに修正

--- src/main/java/sys_practice/Upload.java
+++ src/main/java/sys_practice/Upload.java
@@ -17,36 +17,38 @@ public class Upload {
 	protected String[] fileName = new String[100]; //ファイル名
 
 	/* AWSの接続 */
-	private static Connection getRemoteConnection() throws SQLException {
-	    if (System.getenv("syspractice.crew3xxz5di7.ap-northeast-1.rds.amazonaws.com") != null) {
-	      try {
-	      Class.forName("org.postgresql.Driver");
-	      String dbName = System.getenv("eddb");
-	      String userName = System.getenv("admin");
-	      String password = System.getenv("AraikenR4!");
-	      String hostname = System.getenv("syspractice.crew3xxz5di7.ap-northeast-1.rds.amazonaws.com");
-	      String port = System.getenv("3306");
-	      String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-	      Connection con = DriverManager.getConnection(jdbcUrl);
-	      return con;
-	    }
-	    catch (ClassNotFoundException e) {}
-	    catch (SQLException e) {}
-	    }
-	    return null;
+	private static Connection getRemoteConnection(String dbName) throws SQLException {
+		// JDBCドライバー読み込み
+		try {
+			System.out.println("Loading driver...");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Cannot find the driver in the classpath!", e);
+		}
+
+		String userName = "admin";
+		String password = "AraikenR4!";
+		String hostname = "syspractice.crew3xxz5di7.ap-northeast-1.rds.amazonaws.com";
+		String port = "3306";
+		String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName +
+				"?user=" + userName + "&password=" + password;
+		Connection con = DriverManager.getConnection(jdbcUrl);
+
+		return con;
 	  }
 
 
 	public void uploadMaterial(String materialName, String explanation, int price, String category, String thumbnail, String fileName,  int categoryId, int providerId) throws Exception {
 
+		Connection conn = getRemoteConnection("sys-practice");
+
 		/* categoryIdでその他が選択されているとき */
 		if(categoryId == 0) {
 
-			Connection con = getRemoteConnection();
-
 		    /* categoryに新規カテゴリを追加 */
 		    String sql = "INSERT INTO category (categoryName) VALUES (?)";
-		    PreparedStatement stmt1 = con.prepareStatement(sql);
+		    PreparedStatement stmt1 = conn.prepareStatement(sql);
 		    stmt1.setString(1, category);
 		    stmt1.executeUpdate();
 
@@ -55,21 +57,18 @@ public class Upload {
 
 		    /* 新規カテゴリのIDをcategoryから取り出し */
 		    String sql2 = "SELECT categoryId FROM category WHERE categoryName = ?";
-		    PreparedStatement stmt2 = con.prepareStatement(sql2);
+		    PreparedStatement stmt2 = conn.prepareStatement(sql2);
 		    stmt2.setString(1, category);
 		    ResultSet rs = stmt2.executeQuery();
 		    categoryId = rs.getInt("categoryId");
 
 		    rs.close();
 		    stmt2.close();
-		    con.close();
 	    }
 
 		/* Materialへ素材を追加 */
-		Connection con = getRemoteConnection();
-
 	    String sql = "INSERT INTO Material (materialName,price,thumbnail,categoryId,providerId,explanation) VALUES (?,?,?,?,?,?,?)";
-	    PreparedStatement stmt3 = con.prepareStatement(sql);
+	    PreparedStatement stmt3 = conn.prepareStatement(sql);
 	    stmt3.setString(1, materialName);
 	    stmt3.setInt(2, price);
 	    stmt3.setString(3, thumbnail);
@@ -80,6 +79,6 @@ public class Upload {
 	    stmt3.executeUpdate();
 
 	    stmt3.close();
-	    con.close();
+	    conn.close();
 	}
 }
