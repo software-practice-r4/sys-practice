7184a19ad53a057be492fbdfb1aa7456a307ac18
12h1No <s.rhino15@gmail.com>
2022-07-18 03:43:29 +0900

    add: test用クラスを追加

---
+++ src/main/java/test/TestTrend.java
@@ -0,0 +1,19 @@
+package test;
+
+import sys_practice.Trend;
+
+public class TestTrend {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Trend trend = new Trend();
+		
+		try {
+			trend.getTrend(100);
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+}
